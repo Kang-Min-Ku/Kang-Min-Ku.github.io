b4d7f865816137a90dd3c2e8d3a0254a8c02907f
Kang-Min-Ku <netisen4@gmail.com>
2026-07-21 09:04:12 +0900

    Spec change test vscode CHAT

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,48 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+
+  var $homeLinks = $('.home-jump-link')
+  var $homeSections = $('.home-section-heading')
+
+  function getScrollOffset () {
+    var nav = document.querySelector('.navbar.fixed-top') || document.querySelector('#mainNav')
+    var navHeight = nav ? nav.getBoundingClientRect().height : 0
+
+    return Math.ceil(navHeight + 24)
+  }
+
+  function updateSectionOffsets () {
+    var offset = getScrollOffset()
+
+    $homeSections.css('scroll-margin-top', offset + 'px')
+  }
+
+  function scrollToSection (target) {
+    var offset = getScrollOffset()
+    var targetTop = target.getBoundingClientRect().top + window.pageYOffset - offset
+
+    window.scrollTo({
+      top: targetTop,
+      behavior: 'smooth'
+    })
+  }
+
+  $homeLinks.on('click', function (event) {
+    var hash = this.getAttribute('href')
+    var target = hash ? document.querySelector(hash) : null
+
+    if (!target) {
+      return
+    }
+
+    event.preventDefault()
+    scrollToSection(target)
+
+    if (history.pushState) {
+      history.pushState(null, '', hash)
+    }
+  })
+
+  updateSectionOffsets()
+  $(window).on('resize', updateSectionOffsets)
 })
